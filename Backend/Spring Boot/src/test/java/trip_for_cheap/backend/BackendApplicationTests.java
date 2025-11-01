824b0a8e77756ab297adbefaa7505bbd9207b246
PreaTiziano <prea.t@colegiosanpedro.org.ar>
2025-10-31 21:00:47 -0300

    Comienzo proyecto, creamos carpeta backend y realizamos spring boot

---
+++ Backend/Spring Boot/src/test/java/trip_for_cheap/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package trip_for_cheap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
